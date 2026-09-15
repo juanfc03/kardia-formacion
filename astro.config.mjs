478e5d42f5776467a086600f6b4fbb0d7d7057e9
Juan Fernández Ceacero <juanf.ceacero@gmail.com>
2026-09-15 18:34:15 +0200

    feat(config): configurar astro con sitemap, alias y build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://kardiaformacion.es',
+  trailingSlash: 'always',
+  compressHTML: true,
+
+  build: {
+    inlineStylesheets: 'always',
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [sitemap()],
+});
